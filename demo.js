4d252a7759eed214c4b285ca54c8a6e2f36fcf2f
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-01-22 18:24:31 +0530

    TE-T127: Javasript variables and data types(let,const and var)

--- demo.js
+++ demo.js
@@ -1,2 +1,53 @@
 console.log("hello world");
 console.table({city:"jaipur"});
+
+
+// datatypes
+
+// String
+// Number
+// Boolean
+// BigInt
+//date
+// undefined
+// null
+
+// Object
+// Symbol -unique 
+
+
+
+// var score = 100
+
+let score =102;
+
+let name="Hello";
+
+let isloogedin = false
+
+
+// object
+let Teatype = ["lemon tea","green tea","oolong tea","kadak tea"]
+
+let user={firstname:"Hrishi",lastname:"Thakkar"}
+
+
+let getscore = score;
+
+// we just need to use let while we define the variable then we can use it without that we cannot use let second time on the same variable name
+
+getscore = 500;
+
+console.log(getscore)
+
+
+
+// const used to reserve memory in memory space similar to let
+// nut the difference is that we can not change it
+
+let sal=100
+let bonus=25
+
+let total=sal+bonus
+
+console.log(total)
