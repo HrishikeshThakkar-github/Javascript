89bd045e1054438b892955733445fb687682e0c9
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-01-23 19:47:55 +0530

    TE-T127: Javasript loops

---
+++ loops.js
@@ -0,0 +1,31 @@
+//sum of 1 to 5
+let n=0;
+for(let i=1; i<=5;i++){
+    n=n+i;
+}
+console.log(n);
+let countdown = new Array;
+let i=5;
+while(i>=1){
+    countdown.push(i)
+    i--;
+    
+}
+console.log(countdown);
+
+// let teacollection =[];
+// let tea;
+// do{
+//     tea = prompt(`Enter your favourite tea (type stop to finish)`);
+//     if(tea!=="stop"){teacollection.push(tea)}
+// }while(tea!=="stop")
+
+let total=[];
+i=1;
+do{
+    total.push(i);
+    i++;
+}while(i<=3)
+
+console.log(total);
+    
