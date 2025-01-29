590cddfe01307f357b33409d4e92329d30a125fb
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-01-29 19:49:11 +0530

    TE-T127 PR-57: JS .map , .filter , .reduce

--- advance-concepts.js
+++ advance-concepts.js
@@ -48,10 +48,10 @@
 
  */
 
- //event listeners are heavy and takes up memory and hence it needs to be freed up thus garbage collector is used
- 
+//event listeners are heavy and takes up memory and hence it needs to be freed up thus garbage collector is used
 
- // asynchronous js
+
+// asynchronous js
 
 /*
 
@@ -71,15 +71,153 @@ console.log("end");
 
 //example-2
 
-console.log("start");
+// console.log("start");
 
 
-document.getElementById('changetext').addEventListener("click",function cb(){
-    console.log("call back");
-    
-})
-console.log("end");
+// document.getElementById('changetext').addEventListener("click",function cb(){
+//     console.log("call back");
+
+// })
+// console.log("end");
 
 //here also first start and end will get executed and when button is clicked then call back is printed
 
+//example-3
+
+// console.log("start");
+
+// setTimeout(function cbT(){
+//     console.log("call back setTimeout")
+// },5000);
+
+
+// fetch("https://www.youtube.com/")
+// .then(function cbF(){
+//     console.log("call back fetch");
+
+// })
+
+// console.log("end");
+
+
+
+
+//example-4 
+//this example tells us that it is not necessary that perfectly after ----ms the call back will  get executed , the eventloop will wait untill call stack is empty and then it will take call back from callback queue to call stack and implementation is done this is why there is trustissue with setTimeout
+
+// console.log("start");
+
+// setTimeout(function cbT() {
+//     console.log("call back setTimeout")
+// }, 5000);
+
+// console.log("while starts");
+
+
+// let startdate = new Date().getTime();
+// let enddate = startdate;
+
+// while (enddate <= startdate + 10000) {
+//     enddate = new Date().getTime();
+// }
+
+// console.log("end");
+
+
+// .map .filter and .reduce
+
+
+/*
+//.map it is mainly used to map out each element of an array to transform it
+const arr =[1,2,3,4];
+
+function double(x){
+    return x*2;
+}
+
+const output1 = arr.map(double);
+//implementing using higher order function syntax
+
+const output2 = arr.map(
+    function triple(x){
+        return x*3;
+});
+
+//arrow function
+const output3 = arr.map((x)=>{
+    return x.toString(2);
+});
+
+
+console.log(output1);
+console.log(output2);
+console.log(output3);
+
+*/
+
+/*
+
+//.filter -- filter out values (filterd based upon a logic)
+
+//filter odd values
+
+const arr=[1,2,3,4,5];
+
+function isOdd(x){
+    return x%2;
+}
+const output1 = arr.filter(isOdd);
+
+const output2 = arr.filter((x)=>x>4);
+
+console.log(output1);
+console.log(output2);
+
+*/
+
+
+
+/*
+//.reduce ->
+//          in reduce we have to take the whole array and then come out with a single vlaue 
+
  
+//lets first do this without the use of reduce
+
+const arr =[1,2,3,4,5]
+
+//sum of all numbers of an array
+
+function findsum(arr){
+    let sum=0;
+    for(let i=0;i<arr.length;i++)
+        sum=sum+arr[i]
+    return sum;
+}
+
+console.log(findsum(arr));
+
+
+// const output = arr.reduce(function(accumulated value , current value){
+
+// },0);
+
+const output = arr.reduce (function(acc,curr){
+    acc=acc+curr;
+    return acc;
+},0);
+console.log(output);
+
+const output1 = arr.reduce(function(max, curr){
+    if(curr>max) max=curr;
+    return max;
+},0)
+
+console.log(output1);
+
+
+*/
+
+//extra example of map, reduce and filter 
+
+
